bee38720bbf9574b61428956dd37dc6cb8cfbaab
Erik Gomes <erikgtorres28@gmail.com>
2024-02-15 00:01:13 -0300

    fix: es module

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,4 @@
-import User from "../models/User"
+import User from "../models/User.js"
 import * as Yup from 'yup'
 
 const { v4 } = require("uuid")
--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,7 @@
-module.exports = {
-  dialect: 'postgres',
-  url: 'postgresql://postgres:1cEedcE22C5aF6236bfG1**5bB6*e11C@viaduct.proxy.rlwy.net:49551/railway',
-  // host: 'localhost',
-  // username: 'postgres',
-  // password: 'erikdev',
-  // database: 'codeburger',
-  define: {
-    timestamps: true /* adiciona os campos 'createdAt' e 'updatedAt' automaticamente, ao criar um dado */,
-    underscored: true,
-    underscoredAll: true /* os nomes de cada dado de uma tabela fica no estilo 'snake_case' */,
-  },
-}
+export const dialect = 'postgres';
+export const url = 'postgresql://postgres:1cEedcE22C5aF6236bfG1**5bB6*e11C@viaduct.proxy.rlwy.net:49551/railway';
+export const define = {
+  timestamps: true /* adiciona os campos 'createdAt' e 'updatedAt' automaticamente, ao criar um dado */,
+  underscored: true,
+  underscoredAll: true /* os nomes de cada dado de uma tabela fica no estilo 'snake_case' */,
+};
